d706a45cf29444a962390f35db7ca15845d0c5a2
fandaymon <fandaymon@gmail.com>
2022-12-14 19:14:49 +0200

    Local Tuya Cycle Fix

--- scripts/cycle_local_tuya.php
+++ scripts/cycle_local_tuya.php
@@ -50,7 +50,7 @@ $dps_null = array();
 while (1) {
     if ((time() - $latest_disc) >= 5 * 60) {
         $latest_disc = time();
-        $devices = SQLSelect("SELECT ID, TITLE, LOCAL_KEY, DEV_ID, DEV_IP, d.UUID, '' as MAC, 0 as 'ZIGBEE', SEND12, FLAGS12, VER_3_1 FROM tudevices WHERE LOCAL_KEY!='' and DEV_IP!='' and STATUS=1 ORDER BY DEV_ID");
+        $devices = SQLSelect("SELECT ID, TITLE, LOCAL_KEY, DEV_ID, DEV_IP, UUID, '' as MAC, 0 as 'ZIGBEE', SEND12, FLAGS12, VER_3_1 FROM tudevices WHERE LOCAL_KEY!='' and DEV_IP!='' and STATUS=1 ORDER BY DEV_ID");
         $gw_devices = SQLSelect("SELECT d.ID, d.TITLE, gw.LOCAL_KEY, d.DEV_ID, gw.DEV_IP, d.UUID, d.MAC, 1 as 'ZIGBEE' FROM tudevices d INNER JOIN tudevices gw ON d.MESH_ID = gw.DEV_ID WHERE gw.LOCAL_KEY!='' and gw.DEV_IP!='' and d.STATUS=1");
         $devices = array_merge($devices ,$gw_devices); 
         if ($cycle_debug) {
